f367f785df9905be3995b3f5ee1ab36e025d458d
Carlos E. Tejada <carlos@carlosetejada.com>
2025-01-10 22:17:30 -0500

    Add entry point script for Faramir tool
    The faramir.py script serves as the main entry point for the tool, allowing users to run the program with simple arguments. It integrates with the core functionality implemented in src/main.py.

--- faramir.py
+++ faramir.py
@@ -1,28 +1,5 @@
-from glob import glob
-
-import numpy as np
-import pandas as pd
+from src.main import main
 from clize import run
-from exif import Image
-from datetime import datetime
 
-
-def _update_image_tags (image_path : str, tags):
-	i = Image(image_path)
-
-	i.aperture = tags.Aperture
-	i.shutter_speed_value = tags.Shutter
-	i.focal_length_in_35mm_film = tags[5]
-	i.datetime = datetime.strptime(tags.Date, '%m/%d/%y %I:%M %p')
-	i.gps_latitude = tags.Longitude
-	i.gps_longitude = tags.Latitude
-
-def main(image_folder_path : str, image_info_path : str):
-	info = pd.read_csv(image_info_path, delimiter=',')
-	image_paths = glob(image_folder_path)
-
-	if len(info) != len(image_paths):
-		return -1
-	
-	for image_path, image_info in zip (image_paths, info.itertuples()):
-		_update_image_tags(image_path, image_info)
+if __name__ == "__main__":
+    run(main)
